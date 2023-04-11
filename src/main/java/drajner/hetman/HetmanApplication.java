e038308a4d6a9e12802b9e7e9b720dd8490192a6
Drajner <drajner@wp.pl>
2023-04-11 23:31:38 +0200

    initial commit

---
+++ src/main/java/drajner/hetman/HetmanApplication.java
@@ -0,0 +1,13 @@
+package drajner.hetman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HetmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/drajner/hetman/HetmanApplicationTests.java
@@ -0,0 +1,13 @@
+package drajner.hetman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HetmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
